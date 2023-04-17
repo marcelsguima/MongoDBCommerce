ade1e378704db5cedb9ba8cf625de13c551d8659
marcelsguima <marcelsguima@gmail.com>
2023-04-17 18:35:00 -0300

    Lint Fix Again!

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,16 +1,16 @@
 db.produtos.updateMany(
   {
-    "valoresNutricionais": {
+    valoresNutricionais: {
       $elemMatch: {
-        "tipo": "sódio",
-        "percentual": { $gt: 20, $lt: 40 }
-      }
-    }
+        tipo: "sódio",
+        percentual: { $gt: 20, $lt: 40 },
+      },
+    },
   },
   {
     $addToSet: {
-      "tags": "contém sódio"
-    }
-  }
+      tags: "contém sódio",
+    },
+  },
 );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
